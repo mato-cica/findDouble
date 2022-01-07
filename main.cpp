93eaab30314a2ea26d7c1f610aa190086111af7d
Mato Čiča <mato.cica@gmail.com>
2022-01-07 15:36:04 +0100

    The new findDouble(vector<int>& values) function.
    The new version is using only multiset. There is only one pass through the multiset.

--- main.cpp
+++ main.cpp
@@ -3,77 +3,86 @@
 #include <set>
 
 using namespace std;
+
 void findDoubles(vector<int>& values)
 {
-    set<int> equals;                         // set for members with equals values in vector 'values'
-    set<int>::iterator its;                  // iterator for set 'equals'
 
     multiset<int> valuesMSet;                // multiset will sort vector 'values'
     multiset<int>::iterator it;              // two iterstors for searching for members with equal values in multiset
     multiset<int>::iterator it1;
 
+
+    int iter1;
+
     for(int value : values)                  // initialising multiset with members of vector 'values'
         valuesMSet.insert(value);
 
-    it = valuesMSet.cbegin();                // first iterator pointed to first element of multiset...
-    it1 = valuesMSet.cbegin();
-    ++it1;                                   // ... and second to second element
-
-    while(it1 != valuesMSet.cend())          // start finding members with equal values in the multiset
-    {
-        if(*it == *it1)                      // if first two members equal...
-        {
-            equals.insert(*it);              // ... insert one of them in set 'equals'...
-            ++it1;                           // ... and increment both iterators
-            ++it;
-        }
-        else                                 // ... if first two members are not equal...
-        {
-            ++it1;                           // ... just increment both iterators
-            ++it;
-        }
-    }
-
-    for(int eq : equals)                     // erasing doubles in multiset
-        valuesMSet.erase(eq);
-
-    /** searching for doubles in the multiset **/
-    it = valuesMSet.cbegin();                // set iterators pointing to first two members of multiset
+    it = valuesMSet.cbegin();                 // set iterators pointing to first two members of multiset
     it1 = valuesMSet.cbegin();
     ++it1;
-    while(it1 != valuesMSet.cend())          // iterators passing through the multiset
+
+    while(it1 != valuesMSet.cend())            // Passing through multiset
     {
-        if(*it == 0)                         // if the first member is 0...
+        if(*it == 0 && *it1 != 0)              // If there is only one zero in multiset...
         {
-            cout << *it << " ";              // ... push it to output terminal...
-            ++it;                            // ... then increment iterators
-            ++it1;
+            cout << *it << " ";                // Send it to std out...
+            it++;                              // and increment the pointers
+            it1++;
         }
-        else                                 // if the first member of the multiset is not 0
+        else
         {
-            if(*it * 2 == *it1)              // compare first two members and if second is double of first
+            if(*it == 0 && *it1 == 0)          // If there are more zeros...
             {
-                cout << *it << " ";          // ... push it to output terminal...
-                ++it;                        // ... then increment iterators
-                ++it1;
-            }
-            else                             // if the second member is not double as of first one...
-            {
-                if(*it * 2 > *it1)           // ... check if it is smaller then double as of first one...
-                    ++it1;                   // ... if it is just increment the second iterator (take the next member)
-                else                         // if the second is greather then double as of first one...
+                it++;                          //  just skip them and increment the iterators
+                it1++;
+                while(*it == 0)                // Increment the iterators until 'it' is not pointing to zero
                 {
-                        ++it;                //... just increase both iterators
-                        ++it1;
+                    it++;
+                    it1++;
                 }
             }
+        }
+
+        if(*it * 2 > *it1)                     // If the member is more then 1/2 of next one...
+        {
+            it1++;                             // ...increment 'it1' iterator. Don't touch 'it' iterator until...
+            while(*it * 2 > *it1 )             // ... the multiset member is smaller then 1/2 of one 'it1' pointing to
+                it1++;
+        }
+        else                                   // If the member is exactly 1/2 of the next one...
+        {
+            if(*it * 2 == *it1)
+            {
+                iter1 = *it1;                   // ... check the next of next one
+                it1++;
+                if(*it1 != iter1)               // If the next one is not equal to the previous one...
+                {
+                    cout << *it << " ";         // ... send it to the console...
+                    it++;                       // ... and increment 'it'
+                }
+                else                            // If the next of the next one is ...
+                {                               // ... equal to the previous one...
+                    if(*it1 == iter1)
+                    {
+                        it1++;                  // ... just increment the 'it1' until it is not pointing to...
+                        while(*it1 == iter1)    // ... the member with equal value
+                            it1++;
+                        it++;
+                    }
+                }
+            }
+        }
+        if(*it * 2 < *it1)                      // And finaly if the next member is more then 1/2 of previous one...
+        {
+            it++;                               // ... just increment iterators.
+            it1++;                              // And that is it
         }
     }
 }
 
 int main()
 {
-    vector<int> values{1,2,3,4,5,6,7,8,9,0,8,8,8,9,10,11,9,8,5,1};
+    vector<int> values{1,2,3,4,5,6,7,8,9,0,8,8,8,9,10,11,9,8,5,1,0,0};
     findDoubles(values);
 
     return 0;
